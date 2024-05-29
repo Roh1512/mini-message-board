9b760a5e6ab607879e7056ef234291179568a50d
Rohith19 <rohith1512a@gmail.com>
2024-05-29 14:08:28 +0530

    finished

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 const express = require("express");
 const router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+router.post("/new", function (req, res, next) {
+  const text = req.body.message;
+  const user = req.body.author;
+  const added = new Date();
+  if (text && user) {
+    messages.push({ text: text, user: user, added, added });
+  }
+  res.redirect("/");
 });
 
 module.exports = router;
